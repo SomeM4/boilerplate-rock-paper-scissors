6417ac3377195aa96e621c3fa843d0a973fb953c
SomeM4 <carlosdavin06@gmail.com>
2025-12-17 01:22:26 +0000

    Created Default Opening

--- RPS.py
+++ RPS.py
@@ -3,8 +3,10 @@
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
-
+    # If no history return "Paper" (Beats common "Rock" opening)
+    if len(opponent_history) == 0:
+        return "P" 
+    
+    
+        
     return guess
