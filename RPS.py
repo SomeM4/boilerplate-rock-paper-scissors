40b87991ced7d92eb7744372d909ce34d6b2cd44
SomeM4 <carlosdavin06@gmail.com>
2025-12-17 01:32:39 +0000

    Improve Strategy 2

--- RPS.py
+++ RPS.py
@@ -10,12 +10,20 @@ def player(prev_play, opponent_history=[]):
     # Strategy 1: Beats opponent play last
     last_opp = opponent_history[-1]
     
-    # Strategy 2: If opponent last two move are the same, counter it
+    # Strategy 2: Pattern Detection
     if len(opponent_history) >= 3:
+        # If opponent last two move are the same, counter it
         if opponent_history[-1] == opponent_history[-2]:
             if last_opp == "R": return "P"
             if last_opp == "P": return "S"
             if last_opp == "S": return "R"
+            
+        # Check for cycles
+        recent = opponent_history[-3::]
+        if recent == ["R", "P", "S"] or recent == ["S", "P", "R"]:
+            return "R"
+        elif recent == ["P", "S", "R"] or recent == ["R", "S", "P"]:
+            return "P"
             
     if last_opp == "R":
         return "P"
